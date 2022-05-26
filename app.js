bc014674914e6700b439d417175997ec00cdd5c6
Finson Coutinho <finsoncoutinho@Finsons-MacBook-Air.local>
2022-05-27 00:10:11 +0530

    Md5 Hashing

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const { stringify } = require("querystring");
-const encrypt = require("mongoose-encryption");
+
+const md5 = require("md5");
 
 const app = express();
 
@@ -17,11 +18,6 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-});
-
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
 });
 
 const User = new mongoose.model("User", userSchema);
@@ -38,7 +34,7 @@ app.get("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const userName = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: userName }, function (err, foundUser) {
     if (err) {
@@ -56,7 +52,7 @@ app.post("/login", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser.save(function (err) {
